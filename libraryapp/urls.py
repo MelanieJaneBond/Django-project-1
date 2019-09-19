0da6e038e791edfa59da25c1550704525db6250d
MelanieJaneBond <miss.bond2@gmail.com>
2019-09-19 10:29:22 -0500

    inital commit

---
+++ libraryapp/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.conf.urls import url, include
+from .views import *
+
+app_name = "libraryapp"
+
+urlpatterns = [
+    url(r'^$', book_list, name='home'),
+    url(r'accounts/', include('django.contrib.auth.urls')),
+    url(r'^logout/$', logout_user, name='logout'),
+    url(r'^books$', book_list, name='books'),
+    url(r'^librarians$', librarian_list, name='librarians'),
+    url(r'^libraries$', library_list, name="libraries")
+]
---
+++ libraryapp/views/connection.py
@@ -0,0 +1,2 @@
+class Connection:
+    db_path = "/Users/missb/workspace/python/myawesomeproject/db.sqlite3"
